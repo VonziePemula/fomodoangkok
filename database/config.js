2e62a5dfbf36000a48cc0be5aff8857c5ff7e24b
VonziePemula <vonzie79@gmail.com>
2025-10-08 22:30:55 +0700

    Add files via upload

---
+++ database/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  tokens: "8456541660:AAHGP_jlVBek3meCxP77tkkRdoUgtqfuXWc",  // Ubah Jadi Token Bot Mu !!!
+  owner: "7807425271", // Ubah Jadi Id Mu !!!
+  port: "5000", // Ubah Jadi Port Panel Mu !!!
+  ipvps: "0.0.0.0" // Ubah Jadi Ip Vps Mu !!!
+};
